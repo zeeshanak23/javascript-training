58f8bb51fa2c21c4997e193b52fa142cb8cc8cab
zeeshan@479 <zakhter449@gmai.com>
2023-02-13 10:39:09 +0530

    add ten variable

--- variables/variables.js
+++ variables/variables.js
@@ -1,4 +1,18 @@
 //How to declare variables
-let js = "amazing";
+let jS = "amazing";
+let js_12 = "amazing12";
+let js$12 = "amazing23";
+let js_2 = "amazing45";
+let jsNew = "amazing67";
+let _12_js = "amazing89";
+let js$_12 = "amazing56";
+let js22 = "amazing26";
+let js_Bane = "amazing86";
+let js_ = "amazing576";
+
+
+
+
+
 
 
