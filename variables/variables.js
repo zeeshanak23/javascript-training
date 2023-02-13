f2792ebaff66a7231f793c1fec15d89dbbdf2c69
zeeshan@479 <zakhter449@gmai.com>
2023-02-13 10:55:57 +0530

    Declaring a constant

--- variables/variables.js
+++ variables/variables.js
@@ -28,6 +28,38 @@ console.log(teacherName)
 console.log(accountNo)
 
 
+const c1 = "constan 1";
+const c2 = "constant 2";
+const c3 = "constant 3";
+const c4 = "constant 4";
+const c5 = "constant 5";
+const c6 = "constant 6";
+const c7 = "constant 7";
+const c8 = "constant 8";
+const c9 = "constant 9";
+const c10 = "constant 10";
+
+c1 = "zeeshan 1";
+c2 = "zeeshan 2";
+c3 = "zeeshan 3";
+c4 = "zeeshan 4";
+c5 = "zeeshan 5";
+c6 = "zeeshan 6";
+c7 = "zeeshan 7";
+c8 = "zeeshan 8";
+c9 = "zeeshan 9";
+c10 = "zeeshan 10";
+
+console.log(c1)
+console.log(c2)
+console.log(c3)
+console.log(c4)
+console.log(c5)
+console.log(c6)
+console.log(c7)
+console.log(c8)
+console.log(c9)
+console.log(c10)
 
 
 
