e08b81d0c933549c6a7e4e6357cabbc0f59a7c76
zeeshan@479 <zakhter449@gmai.com>
2023-02-14 10:12:30 +0530

    changes in folder variable and operator

--- operators/arithmetic.js
+++ operators/arithmetic.js
@@ -1,5 +1,6 @@
 /**
  * Divide two numbers (5 different pairs)
+ * 
  *
  * Multiply two numbers (5 different pairs)
  *
@@ -30,3 +31,133 @@
  * and four different operators '+', '-', '*','/'
  * using the variable - a, b, c, d
  */
+
+
+// Divide two number
+a = 20 / 2;
+console.log(a)
+
+c = 50 / 4;
+console.log(c)
+
+e = 70 / 12;
+console.log(e)
+
+g = 40 / 9;
+console.log(g)
+
+i = 90 / 9;
+console.log(i)
+
+// Multiply two number
+num1 = 22 * 5;
+console.log(num1)
+
+num2 = 42 * 6;
+console.log(num2)
+
+num3 = 31 * 9;
+console.log(num3)
+
+num4 = 29 * 7;
+console.log(num4)
+
+num5 = 56 * 8;
+console.log(num5)
+
+// Substract two numbers
+sub_1 = 90 - 5;
+console.log(sub_1)
+
+sub_2 = 78 - 65;
+console.log(sub_2)
+
+sub_3 = 67 - 45;
+console.log(sub_3)
+
+sub_4 = 67 - 56;
+console.log(sub_4)
+
+sub_5 = 89 - 89;
+console.log(sub_5)
+
+// Find remmainder of two numbers
+rem_1 = 43 / 4;
+console.log(rem_1)
+
+rem_2 = 56 / 5;
+console.log(rem_2)
+
+rem_3 = 92 / 9;
+console.log(rem_3)
+
+rem_4 = 87 / 6;
+console.log(rem_4)
+
+rem_5 = 90 / 8;
+console.log(rem_5)
+
+// Divide and then multiply
+divMul_1 = (24 / 2) * 5;
+console.log(divMul_1)
+
+dm_2 = (24 / 12) * 10;
+console.log(dm_2)
+
+dm_3 = (22 / 2) * 2;
+console.log(dm_3)
+
+dm_4 = (90 / 5) * 10;
+console.log(dm_4)
+
+dm_5 = (77 / 7) * 8;
+console.log(dm_5)
+
+// Multiply and then substract
+ms_1 = (78 * 10) - 100;
+console.log(ms_1)
+
+ms_2 = (65 * 10) - 50;
+console.log(ms_2)
+
+ms_3 = (56 * 25) - 90;
+console.log(ms_3)
+
+ms_4 = (78 * 8) - 200;
+console.log(ms_4)
+
+ms_5 = (56 * 5) - 50;
+console.log(ms_5)
+
+
+// Find square of numbers 2 - 10
+
+function square(number) {
+    return number ** 2;
+}
+console.log(square(2))
+console.log(square(3))
+console.log(square(4))
+console.log(square(5))
+console.log(square(6))
+console.log(square(7))
+console.log(square(8))
+console.log(square(9))
+console.log(square(10))
+
+
+// Operator precedence
+a = (2 * 2) + 5;
+console.log(a)
+
+b = (20 / 3) * 3;
+console.log(b)
+
+c = (89 - 65) + 23;
+console.log(c)
+
+d = (23 + 45) * 25;
+console.log(d)
+
+e = (56 / 34) - 2;
+console.log(e)
