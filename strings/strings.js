8f14acc3e6d99fe9729c3f319d1fb4e1b5c944b2
zeeshan@479 <zakhhter449@gmail.com>
2023-02-21 09:16:56 +0530

    add string

---
+++ strings/strings.js
@@ -0,0 +1,26 @@
+// Create a variable `temperature` and set the value of 30℃ and convert into Fahrenheit. 
+// The value of the variable should be printed as follows.
+
+let temperature = 30;
+let temp = (9 / 5 * temperature) + 32;
+
+console.log(`The current temperature is fahrenheit is ` + temp + `f`)
+
+
+// Create two variables - `rectLength` and `rectBreadth` and initialize with some number.
+//  Find the area of the rectangle, and print as follows
+
+let rectLength = 30;
+let rectBreath = 30;
+console.log(`The area of the rectangle is ` + rectLength * rectBreath + ` square foot`)
+
+// Create a variable which stores multi line string and print it.
+
+let sentence2 = `Hello Everyone \n today i focused my 100% attention in the classroom`
+console.log(sentence2)
+
+// Use back tick **`** for using variables inside a string and **${}** for referencing the variables.
+let name = "zeeshan"
+console.log(`My name is ${name}`)
+
+
